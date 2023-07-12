e7caaa41be48fbe83bdd31ba1060efa29d72e821
xhnya <930957853@qq.com>
2023-07-12 20:25:13 +0800

    代码升级，重构代码

--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-
-import { createRouter,createWebHashHistory  } from 'vue-router'
-import routers from "@/router/router";
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes: routers
-})
-
-export default router
